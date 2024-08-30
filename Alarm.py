54c0c32b9cc7337974d361adaefd25648bc68ebf
Nallin-kumar <158268020+Nallin-kumar@users.noreply.github.com>
2024-08-30 20:23:16 +0530

    Add files via upload

---
+++ Alarm.py
@@ -0,0 +1,29 @@
+#Credits = "Bro Code"
+#import all the files which are necessary
+
+import time
+import datetime
+import pygame
+
+def set_alarm(alarm_time):
+    print(f"Alarm set for {alarm_time} ")
+    sound_file = "BMO - Dyalla.mp3"  # Download the music file and refact it into the main file
+    is_running = True
+    while is_running:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        print(current_time)
+        if current_time == alarm_time:
+            print("Wake UP !")
+            pygame.mixer.init()
+            pygame.mixer.music.load(sound_file)
+            pygame.mixer.music.play()
+
+            while pygame.mixer.music.get_busy():
+                time.sleep(1)
+
+            is_running=False
+        time.sleep(1)
+
+if __name__=="__main__":
+    alarm_time = input("Enter the alarm time (HH:MM:SS) ")
+    set_alarm(alarm_time)
